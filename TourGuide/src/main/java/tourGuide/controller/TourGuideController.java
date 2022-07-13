b8603d6850f0232cd6155d22bb00c5e0c1c08684
Dixen7 <dixenlive@gmail.com>
2022-07-13 14:10:26 +0200

    implement all endpoint in controller, refacto service

--- TourGuide/src/main/java/tourGuide/controller/TourGuideController.java
+++ TourGuide/src/main/java/tourGuide/controller/TourGuideController.java
@@ -2,6 +2,8 @@ package tourGuide.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,71 +12,79 @@ import org.springframework.web.bind.annotation.RestController;
 import com.jsoniter.output.JsonStream;
 
 import gpsUtil.location.VisitedLocation;
+import tourGuide.model.user.UserReward;
 import tourGuide.service.TourGuideService;
 import tourGuide.model.user.User;
+import tourGuide.service.UserService;
 import tripPricer.Provider;
 
 @RestController
 public class TourGuideController {
 
-	@Autowired
-	TourGuideService tourGuideService;
+    private Logger logger = LoggerFactory.getLogger(TourGuideController.class);
+    @Autowired
+    TourGuideService tourGuideService;
+
+    @Autowired
+    UserService userService;
 	
     @RequestMapping("/")
     public String index() {
         return "Greetings from TourGuide!";
     }
     
-    @RequestMapping("/getLocation") 
+    @RequestMapping("/getLocation")
     public String getLocation(@RequestParam String userName) {
-    	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
-		return JsonStream.serialize(visitedLocation.location);
+        logger.info("getLocation: endpoint called.");
+        VisitedLocation visitedLocation = tourGuideService.getUserLocation(userName);
+        if (visitedLocation == null) {
+            logger.info("getLocation: requested user not found.");
+            return JsonStream.serialize("User Not Found [" + userName + "]");
+        }
+        logger.info("getLocation: returning user location.");
+        return JsonStream.serialize(visitedLocation.location);
     }
-    
-    //  TODO: Change this method to no longer return a List of Attractions.
- 	//  Instead: Get the closest five tourist attractions to the user - no matter how far away they are.
- 	//  Return a new JSON object that contains:
-    	// Name of Tourist attraction, 
-        // Tourist attractions lat/long, 
-        // The user's location lat/long, 
-        // The distance in miles between the user's location and each of the attractions.
-        // The reward points for visiting each Attraction.
-        //    Note: Attraction reward points can be gathered from RewardsCentral
-    @RequestMapping("/getNearbyAttractions") 
+
+    @RequestMapping("/getNearbyAttractions")
     public String getNearbyAttractions(@RequestParam String userName) {
-    	VisitedLocation visitedLocation = tourGuideService.getUserLocation(getUser(userName));
-    	return JsonStream.serialize(tourGuideService.getNearByAttractions(visitedLocation));
+        logger.info("getNearbyAttractions: endpoint called.");
+        VisitedLocation visitedLocation = tourGuideService.getUserLocation(userName);
+        if (visitedLocation == null) {
+            logger.info("getNearbyAttractions: requested user not found.");
+            return JsonStream.serialize("User Not Found [" + userName + "]");
+        }
+        logger.info("getNearbyAttractions: returning nearby attractions.");
+        return JsonStream.serialize(tourGuideService.getNearByAttractions(visitedLocation));
     }
     
-    @RequestMapping("/getRewards") 
+    @RequestMapping("/getRewards")
     public String getRewards(@RequestParam String userName) {
-    	return JsonStream.serialize(tourGuideService.getUserRewards(getUser(userName)));
+        logger.info("getRewards: endpoint called.");
+        List<UserReward> userRewards = tourGuideService.getUserRewards(userName);
+        if (userRewards == null) {
+            logger.info("getRewards: requested user not found.");
+            return JsonStream.serialize("User Not Found [" + userName + "]");
+        }
+        logger.info("getRewards: returning user rewards.");
+        return JsonStream.serialize(userRewards);
     }
     
     @RequestMapping("/getAllCurrentLocations")
     public String getAllCurrentLocations() {
-    	// TODO: Get a list of every user's most recent location as JSON
-    	//- Note: does not use gpsUtil to query for their current location, 
-    	//        but rather gathers the user's current location from their stored location history.
-    	//
-    	// Return object should be the just a JSON mapping of userId to Locations similar to:
-    	//     {
-    	//        "019b04a9-067a-4c76-8817-ee75088c3822": {"longitude":-48.188821,"latitude":74.84371} 
-    	//        ...
-    	//     }
-    	
-    	return JsonStream.serialize("");
+        logger.info("getAllCurrentLocations: endpoint called. Returning all current locations");
+        return JsonStream.serialize(tourGuideService.getAllCurrentLocations());
     }
     
     @RequestMapping("/getTripDeals")
     public String getTripDeals(@RequestParam String userName) {
-    	List<Provider> providers = tourGuideService.getTripDeals(getUser(userName));
-    	return JsonStream.serialize(providers);
+        logger.info("getTripDeals: endpoint called.");
+        List<Provider> providers = tourGuideService.getTripDeals(userName);
+        if (providers == null) {
+            logger.info("getTripDeals: requested user not found.");
+            return JsonStream.serialize("User Not Found [" + userName + "]");
+        }
+        logger.info("getTripDeals: returning trip deals for user.");
+        return JsonStream.serialize(providers);
     }
-    
-    private User getUser(String userName) {
-    	return tourGuideService.getUser(userName);
-    }
-   
 
 }
